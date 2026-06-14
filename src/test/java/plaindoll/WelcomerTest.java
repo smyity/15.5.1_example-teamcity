de8f1bc18a3bf92f4fda136862e24fc97cfeb9c5
osho <osho_osho@mail.ru>
2026-06-14 18:07:38 +0000

    add hunt

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WelcomerTest {
+
+	private Welcomer welcomer = new Welcomer();
+
 	@Test
 	public void welcomerSaysWelcome() {
 		assertThat(welcomer.sayWelcome(), containsString("Welcome"));
